a95ef18be69550fb1848bf69eab845c164f1870d
Jed Rembold <jedediah.r@gmail.com>
2026-04-08 00:50:18 -0700

    Clarify need of positive offsets for testing

--- Prob2.py
+++ Prob2.py
@@ -10,7 +10,11 @@ def invert_wiring_offsets(r2l_offsets):
 
 # Unit test
 def test_invert_wiring_offsets():
-    """Tests several R2L and resulting L2R wiring offset lists"""
+    """
+    Tests several R2L and resulting L2R wiring offset lists
+
+    NOTE THAT THESE TESTS WILL ONLY WORK IF YOU RETURN POSITIVE OFFSETS!
+    """
     assert invert_wiring_offsets([0]*26) == [0]*26
     wiring_offsets = [3,24,13,14,2,25,3,15,11,17,6,25,22,24,7,16,17,11,0,21,7,18,16,23,0,24]
     rev_wiring_offsets = [23,2,13,12,24,1,23,11,15,9,20,1,4,2,19,10,9,15,0,5,19,8,10,3,0,2]
